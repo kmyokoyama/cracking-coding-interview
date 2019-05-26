8b62d8177704cd57c49aa2b8d0adf4a4328824e6
Kazuki Yokoyama <kmyokoyama@inf.ufrgs.br>
2019-05-26 15:04:01 -0300

    Add question 1 of Chapter 1

---
+++ src/main/java/br/kmyokoyama/cracking/chapter1/UniqueCharacterChecker.java
@@ -0,0 +1,20 @@
+package br.kmyokoyama.cracking.chapter1;
+
+public class UniqueCharacterChecker {
+
+    public boolean check(String text) {
+        int n = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            int charPosition = (int) text.charAt(i); // It uses the ASCII code of the char as its position.
+
+            if ((n & (1 << charPosition)) != 0) { // Note that > 0 would do as well.
+                return false;
+            }
+
+            n |= 1 << charPosition; // Set bit in that position.
+        }
+
+        return true;
+    }
+}
---
+++ src/test/java/br/kmyokoyama/cracking/chapter1/Question1Test.java
@@ -0,0 +1,36 @@
+package br.kmyokoyama.cracking.chapter1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Question1Test {
+
+    @ParameterizedTest(name = "{index} - All chars in \"{0}\" are unique: {1}")
+    @MethodSource("provideStringsForChecker")
+    @DisplayName("Check if string contains only unique chars")
+    public void shouldReturnTrueWhenAllUniqueCharacters(String text, boolean expected) {
+        // Given.
+        UniqueCharacterChecker sut = new UniqueCharacterChecker();
+
+        // When.
+        boolean isUnique = sut.check(text);
+
+        // Then.
+        assertEquals(isUnique, expected);
+    }
+
+    private static Stream<Arguments> provideStringsForChecker() {
+        return Stream.of(
+                Arguments.of("abcd efgh", true),
+                Arguments.of("abcd efah", false),
+                Arguments.of("", true),
+                Arguments.of("  ", false)
+        );
+    }
+}
